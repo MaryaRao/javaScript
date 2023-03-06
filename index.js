f670d7e2f2375bd30e20e2a45e5979b3f5c4a961
MaryaRao <maryarao30@gmail.com>
2023-03-06 23:45:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// console.log('hello',5*6,'input');
+// let x=3;
+// console.log(x);
+// let name= 'Ali';
+// console.log(name);
+// let y=2;
+// console.log(y);
+// let a=2;
+// let b=3;
+// let c= a+b;
+// console.log(c);
+// const pi=3.14;
+// console.log(pi);
+// document.getElementById('para').innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi nam vero odit non fuga, assumenda numquam vitae, amet exercitationem incidunt quas, asperiores ad provident praesentium repudiandae odio voluptatibus magni nesciunt maiores quia ex? Cupiditate eum sapiente qui. Odio aut, assumenda, mollitia tempora accusamus perferendis recusandae provident fuga at quos, minima iusto? Maxime aliquid dolorem necessitatibus, nisi odio autem id nulla. Saepe laborum harum quisquam dolore molestias beatae inventore! Sapiente molestiae illum, similique sit excepturi eius rem fugit aspernatur quod impedit distinctio nulla nesciunt obcaecati quasi modi nemo reprehenderit, voluptas iste sequi, quibusdam magnam sunt repellat? Veritatis quidem quisquam omnis? Voluptatem.";
+// // console.clear();
+// let age= 18;
+// if(age >= 18 && age<=60){
+//     document.write('You are eligible');
+// }
+// else{
+//     document.write('you are not eligible')
+// };
+// let a=+prompt('Enter value of a');
+// let b=+prompt('Enter value of b');
+// let c=+prompt('Enter value of c');
+// if(b>a && b>c){
+//     document.write('b is greater');
+// }
+// else if(c>a && c>b){
+//     document.write('c is greater')
+// }
+// else{
+//     document.write('Error')
+// }
+let day=7;
+switch(day){
+    case 1:document.write('Monday');
+    break;
+    case 2:document.write('Tuesday');
+    break;
+    case 3:document.write('wednesday');
+    break;
+    case 4:document.write('Thursday');
+    break;
+    default:document.write('enter valid day')
+
+}
